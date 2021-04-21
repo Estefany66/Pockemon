6bd362160e819ccef288820e065607ee651f0eb4
Estefany Calatayud <estefany_calatayud@usmp.pe>
2021-04-20 19:56:30 -0500

    nuevo

---
+++ pokemon/Models/Pokemon.cs
@@ -0,0 +1,10 @@
+namespace pokedex.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Ciudad { get; set; }
+       
+    }
+}
